bd3fb921a426f6bd59e1a148965017bdccd49a3b
Muhammad Nafi' Maula Hakim <nafimaulahakim123@gmail.com>
2023-03-22 23:54:35 +0700

    statistik & promo

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -175,13 +175,18 @@
                         <div class="row">
                             <div class="col-7">
                                 @if ($item->is_promo == 1)
-                                    <p class="fw-bold fs-6 m-0 text-capitalize d-flex">
+                                    <div class="mb-2 d-flex align-items-center">
+                                        <div class="bg-light-danger fs-7 p-1 rounded-3 px-2 me-2" style="width: fit-content !important">
+                                            Promo
+                                        </div>
+                                        <div class="bg-light-warning fs-7 p-1 rounded-3 px-2" style="width: fit-content !important">
+                                            Hemat {{ numberFormat($item->price - $item->price_promo,0) }}
+                                        </div>
+                                    </div>
+                                    <p class="fw-bold fs-6 m-0 mb-2 text-capitalize">
                                         <span class="me-2">
                                             {{ $item->name }}
                                         </span>
-                                        <div class="bg-light-danger fs-7 p-1 rounded-3 px-2" style="width: fit-content !important">
-                                            Promo
-                                        </div>
                                     </p>
                                 @else
                                     <p class="fw-bold fs-6 m-0 text-capitalize">{{ $item->name }}</p>
